9d7c0e1a94fae831eb49d2f7b61fc611d33d7f10
Ivan Buhov <ivanbuhov@gmail.com>
2020-09-22 14:52:24 +0300

    Setup TestApp

--- examples/TasksApp/index.js
+++ examples/TasksApp/index.js
@@ -1,3 +1,10 @@
 let tasks = require('tasks');
-console.log("TestApp Console application: ");
-console.log("TestApp: " + JSON.stringify(tasks));
+
+let task1 = new tasks.Task("Go to school", "I have to go to school tomorow, otherwise my mom will kill me.", false);
+let task2 = new tasks.Task("Shopping", "Buy milk, bread, water and candies.", false);
+let task3 = new tasks.Task("Тeeth washing", "I have to wash my teeth twice a day for a perfect smile :).", true);
+let task4 = new tasks.Task("Do my homework", "I don't like it but I have to do it.", false);
+let task5 = new tasks.Task("Go to gym", "", false);
+console.log("task1.name: " + task1.title);
+console.log("task2.toString(): " + task2.toString());
+console.log("task3: " + task3.title + "/" + task3.details + "/" + task3.isCompleted);
