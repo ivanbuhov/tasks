c6de39cb31d6bf4c0b397d80c4cf8c1268b355f9
Ivan Buhov <ivanbuhov@gmail.com>
2020-09-22 22:30:09 +0300

    Add save/load functionallity in JS

--- tasks_addon/src/js/index.js
+++ tasks_addon/src/js/index.js
@@ -1,4 +1,27 @@
+let fs = require("fs");
 let tasks = require('../../build/Release/tasks_addon');
-console.log("tasks/index.js module: ");
-console.log("Module: " + JSON.stringify(tasks));
+
+// TODO: Async version of loadFrom/saveTo are nice to have
+tasks.TasksList.prototype.saveTo = function(filePath) {
+    fs.writeFileSync(filePath, JSON.stringify(this) , 'utf8');
+};
+
+tasks.TasksList.prototype.loadFrom = function(filePath) {
+    let tasksJson = JSON.parse(fs.readFileSync(filePath));
+
+    for (let i = 0; i < tasksJson.length; i++) {
+        this.add(new tasks.Task(tasksJson[i].title, tasksJson[i].details, tasksJson[i].isCompleted));
+    }
+};
+
+//TODO: Figure out why the native implementation of toJSON is not working and once fixed - remove this one.
+tasks.TasksList.prototype.toJSON = function() {
+    let tasksArray = [];
+    for (let i = 0; i < this.length; i++) {
+        tasksArray.push(this.at(i));
+    }
+
+    return tasksArray;
+};
+
 module.exports = tasks;
